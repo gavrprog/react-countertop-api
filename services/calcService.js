833d9a7c6592da70f1cfe7e1bb662c44e9d99d70
51444218+gavrprog@users.noreply.github.com <gavr.prog@gmail.com>
2025-12-31 19:57:26 +0100

    corrected all for production

--- services/calcService.js
+++ services/calcService.js
@@ -1,143 +1,159 @@
-const connection = require('../db')
+const connection = require("../db");
 
 /**
  * расчет столешницы
- * @param {*} param0 
- * @returns 
+ * @param {*} param0
+ * @returns
  */
-async function calculate({ shape, dimentions, stone, chamfer, additional }){
-    let lengthsSum = 0
-    let lengthToCut = 0
-    let width = 0 //ширина столешницы, которую ввел пользователь
-    let arrayLengths = [] //длины, которые ввел пользователь
-    // let slab = {} //стоимость и длина
-    const slab = await getLengthSlab(stone) //получаем цены и длину листа
-    const {chamferPrice, processPrice} = await getAllPrice(chamfer)
-    //поправочное слогаемое, чтобы правильно вычислить длину фаски
-    //длины столешницы складываются и прибавляется два торца
-    //если столешница не прямая, то в суммарной длине столешницы уже сидит как минимум один торец
-    const correction = (Object.values(dimentions).filter(value => value !== '').length - 2 ) * 2
+async function calculate({ shape, dimentions, stone, chamfer, additional }) {
+  if (
+    shape === "multicorner" &&
+    +dimentions.length_c < +dimentions.length_a * 2 + 500
+  ) {
+    return { message: 'Значение размера "С" слишком мало.' };
+  }
+  let lengthsSum = 0;
+  let lengthToCut = 0;
+  let width = 0; //ширина столешницы, которую ввел пользователь
+  let arrayLengths = []; //длины, которые ввел пользователь
+  const slab = await getLengthSlab(stone); //получаем цены и длину листа
+  const { chamferPrice, processPrice } = await getAllPrice(chamfer);
+  //поправочное слогаемое, чтобы правильно вычислить длину фаски
+  //длины столешницы складываются и прибавляется два торца
+  //если столешница не прямая, то в суммарной длине столешницы уже сидит как минимум один торец
+  const correction =
+    (Object.values(dimentions).filter((value) => value !== "").length - 2) * 2;
 
-    if (stone.producer && stone.color) {
-        width = +dimentions.length_a
-        for(length of Object.keys(dimentions)){
-            if(length !== 'length_a'){
-                arrayLengths.push(+dimentions[length])
-                lengthsSum += +dimentions[length]
-            }
-        }   
+  if (stone.producer && stone.color) {
+    width = +dimentions.length_a;
+    for (length of Object.keys(dimentions)) {
+      if (length !== "length_a") {
+        arrayLengths.push(+dimentions[length]);
+        lengthsSum += +dimentions[length];
+      }
     }
-    //sustract from C and D 600mm
-    lengthToCut += 2 * (arrayLengths[0] + +dimentions.length_a) / 1000
-    for (let i = 1; i < arrayLengths.length; i++){
-        arrayLengths[i] -= 600
-        lengthToCut += 2 * (arrayLengths[i] + +dimentions.length_a) / 1000
-    }
-    const lengthChamfer = (lengthsSum + width * (2 - correction)) / 1000
-    const countSlab = countQuantitySlabs(slab, arrayLengths) * 0.5
-    const deliveryHalf = (countSlab - Math.trunc(countSlab)) / 0.5
+  }
+  //substract 600mm from C and D
+  lengthToCut += (2 * (arrayLengths[0] + +dimentions.length_a)) / 1000;
+  for (let i = 1; i < arrayLengths.length; i++) {
+    arrayLengths[i] -= 600;
+    lengthToCut += (2 * (arrayLengths[i] + +dimentions.length_a)) / 1000;
+  }
+  const lengthChamfer = (lengthsSum + width * (2 - correction)) / 1000;
+  const countSlab = countQuantitySlabs(slab, arrayLengths) * 0.5;
+  const deliveryHalf = (countSlab - Math.trunc(countSlab)) / 0.5;
 
-    const sum =
-        countSlab * slab.price + 
-        lengthToCut * processPrice.cutting +
-        lengthChamfer * chamferPrice +
-        processPrice.cuttingFigure * 2 +
-        (additional.sink ? 1 : 0) * (processPrice.cuttingWashing + processPrice.drillWaterTap) +
-        (additional.montage ? 1 : 0) * lengthsSum /1000 * processPrice.mountCountertop +
-        (additional.delivery ? 1 : 0) * processPrice.deliveryProduct +
-        deliveryHalf * processPrice.deliveryStoneHalf +
-        Math.trunc(countSlab) * processPrice.deliveryStoneOne
+  const sum =
+    countSlab * slab.price +
+    lengthToCut * processPrice.cutting +
+    lengthChamfer * chamferPrice +
+    processPrice.cuttingFigure * 2 +
+    (additional.sink ? 1 : 0) *
+      (processPrice.cuttingWashing + processPrice.drillWaterTap) +
+    (((additional.montage ? 1 : 0) * lengthsSum) / 1000) *
+      processPrice.mountCountertop +
+    (additional.delivery ? 1 : 0) * processPrice.deliveryProduct +
+    deliveryHalf * processPrice.deliveryStoneHalf +
+    Math.trunc(countSlab) * processPrice.deliveryStoneOne;
 
-        // console.log(countSlab * slab.price,
-        //     lengthToCut, processPrice.cutting, 
-        //     lengthChamfer,chamferPrice, 
-        //     processPrice.cuttingFigure * 2, 
-        //     (additional.sink ? 1 : 0) * (processPrice.cuttingWashing + processPrice.drillWaterTap),
-        // (additional.montage ? 1 : 0) * lengthsSum /1000 * processPrice.mountCountertop,
-        // (additional.delivery ? 1 : 0) * processPrice.deliveryProduct,
-        // deliveryHalf , processPrice.deliveryStoneHalf, 
-        // Math.trunc(countSlab),processPrice.deliveryStoneOne
-        // )
+  // console.log(countSlab * slab.price,
+  //     lengthToCut, processPrice.cutting,
+  //     lengthChamfer,chamferPrice,
+  //     processPrice.cuttingFigure * 2,
+  //     (additional.sink ? 1 : 0) * (processPrice.cuttingWashing + processPrice.drillWaterTap),
+  // (additional.montage ? 1 : 0) * lengthsSum /1000 * processPrice.mountCountertop,
+  // (additional.delivery ? 1 : 0) * processPrice.deliveryProduct,
+  // deliveryHalf , processPrice.deliveryStoneHalf,
+  // Math.trunc(countSlab),processPrice.deliveryStoneOne
+  // )
 
-    return sum
+  return sum;
 }
 
 /**
  * возвращает количество половинок листов
- * @param {*} slab 
- * @param {*} arrayLengths 
- * @returns 
+ * @param {*} slab
+ * @param {*} arrayLengths
+ * @returns
  */
 function countQuantitySlabs(slab, arrayLengths) {
-    let count = 0;
-    let ratio = 0     
-    let length = slab.length - 20 //уменьшение на ширину торцевки
-    let userLength = 0;
-    let currentRemainder = 0;
-    let arrayRemainders = []; // массив остатков
+  let count = 0;
+  let ratio = 0;
+  let length = slab.length - 20; //уменьшение на ширину торцевки
+  let userLength = 0;
+  let currentRemainder = 0;
+  let arrayRemainders = []; // массив остатков
 
-    arrayLengths.sort((a, b) => b - a);//sort from max to min
+  arrayLengths.sort((a, b) => b - a); //sort from max to min
 
-    while (arrayLengths.length != 0) {
-        userLength = arrayLengths.shift()//get and delete first userLength from arrayUserLength
-        for (let i = 0; i < arrayRemainders.length; i++) {
-            if (arrayRemainders[i] >= userLength) {
-                currentRemainder = arrayRemainders[i] - userLength;
-                userLength = 0;
-                arrayRemainders.splice(i, 1);
-                break;
-            }
-        }
-
-        if (currentRemainder === 0 && userLength != 0) {
-            currentRemainder = length;
-            if (currentRemainder >= userLength) {				
-                currentRemainder -= userLength;
-                count += 1;
-            } else {
-                ratio =  userLength / length;
-                currentRemainder = length * (1 - (ratio - Math.trunc(ratio)));
-                count += Math.trunc(ratio) + 1;
-            }
-        }
-
-        arrayRemainders.push(currentRemainder);
-        arrayRemainders.sort((a, b) => a - b);
-        currentRemainder = 0;
+  while (arrayLengths.length != 0) {
+    userLength = arrayLengths.shift(); //get and delete first userLength from arrayUserLength
+    for (let i = 0; i < arrayRemainders.length; i++) {
+      if (arrayRemainders[i] >= userLength) {
+        currentRemainder = arrayRemainders[i] - userLength;
+        userLength = 0;
+        arrayRemainders.splice(i, 1);
+        break;
+      }
     }
-    return count;
+
+    if (currentRemainder === 0 && userLength != 0) {
+      currentRemainder = length;
+      if (currentRemainder >= userLength) {
+        currentRemainder -= userLength;
+        count += 1;
+      } else {
+        ratio = userLength / length;
+        currentRemainder = length * (1 - (ratio - Math.trunc(ratio)));
+        count += Math.trunc(ratio) + 1;
+      }
+    }
+
+    arrayRemainders.push(currentRemainder);
+    arrayRemainders.sort((a, b) => a - b);
+    currentRemainder = 0;
+  }
+  return count;
 }
 
 /**
  * по поступившим данным выбирает длину листа и его стоимость
- * @param {*} data 
- * @returns 
+ * @param {*} data
+ * @returns
  */
-async function getLengthSlab(stone){
-    const data = await connection.promise().query('SELECT * FROM ' + `${stone.producer}` + ' WHERE ' + 'color=?', [stone.color])    
-    const {normalLength, jamboLength, normlPrice, jamboPrice} = data[0][0]
-    if(normalLength){
-        return {
-            price: normlPrice,
-            length: normalLength
-        }
-    } else if (jamboLength){
-        return {
-            price: jamboPrice,
-            length: jamboLength
-        }
-    }
+async function getLengthSlab(stone) {
+  const data = await connection
+    .promise()
+    .query("SELECT * FROM " + `${stone.producer}` + " WHERE " + "color=?", [
+      stone.color,
+    ]);
+  const { normalLength, jamboLength, normalPrice, jamboPrice } = data[0][0];
+  if (normalLength) {
+    return {
+      price: normalPrice,
+      length: normalLength,
+    };
+  } else if (jamboLength) {
+    return {
+      price: jamboPrice,
+      length: jamboLength,
+    };
+  }
 }
 
-async function getAllPrice(chamfer){
-    const chamferPriceArray = await connection.promise().query('SELECT price FROM chamfers WHERE type=?', [chamfer])
-    const chamferPrice = chamferPriceArray[0][0].price
-    const processPriceArray = await connection.promise().query('SELECT * FROM processingPrice')
-    const processPrice = processPriceArray[0].reduce((processes, process) => {
-        processes[process.process] = process.price;
-        return processes;
-    }, {});
-    return {chamferPrice, processPrice}  
+async function getAllPrice(chamfer) {
+  const chamferPriceArray = await connection
+    .promise()
+    .query("SELECT price FROM chamfers WHERE type=?", [chamfer]);
+  const chamferPrice = chamferPriceArray[0][0].price;
+  const processPriceArray = await connection
+    .promise()
+    .query("SELECT * FROM processingPrice");
+  const processPrice = processPriceArray[0].reduce((processes, process) => {
+    processes[process.process] = process.price;
+    return processes;
+  }, {});
+  return { chamferPrice, processPrice };
 }
 
-module.exports = {calculate}
+module.exports = { calculate };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-const calcService = require('../services/calcService')
-require('dotenv').config({ path: `../.env.${process.env.NODE_ENV || 'development'}` });
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser'); //to convert the body of incoming requests into JavaScript objects
-const cors = require('cors'); //to configure Express to add headers stating that this API accepts requests coming = other origins
-const helmet = require('helmet'); // helps to secure Express APIs by defining various HTTP headers
-const morgan = require('morgan'); // adds some logging capabilities to this Express API
+const calcService = require("../services/calcService");
+require("dotenv").config({
+  path: `../.env.${process.env.NODE_ENV || "development"}`,
+});
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser"); //to convert the body of incoming requests into JavaScript objects
+const cors = require("cors"); //to configure Express to add headers stating that this API accepts requests coming = other origins
+const helmet = require("helmet"); // helps to secure Express APIs by defining various HTTP headers
+const morgan = require("morgan"); // adds some logging capabilities to this Express API
 //const config = require('../config');
-const connection = require('../db')
+const connection = require("../db");
 
 const PORT = process.env.SERVER_PORT || 3001;
 
-const legalProducers = [
-  'avant',
-  'caesarstone'
-]
+const legalProducers = ["avant", "caesarstone", "atem"];
 
 // defining the Express app
 const app = express();
@@ -29,65 +28,74 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // adding morgan to log HTTP requests
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 // обязательно, чтобы читать req.body в POST
 app.use(express.json());
 
 // Раздача статических файлов React (предполагаем, что build в папке /build)
-if (process.env.NODE_ENV === 'production') {
-  const buildPath = path.join(__dirname, '../build');
+if (process.env.NODE_ENV === "production") {
+  const buildPath = path.join(__dirname, "../build");
   app.use(express.static(buildPath));
-  app.get('*', (req, res) => {
+  app.get("*", (req, res) => {
     // Все остальные запросы — отдаем index.html для SPA
-    res.sendFile(path.join(buildPath, 'index.html'));
+    res.sendFile(path.join(buildPath, "index.html"));
   });
 }
 
-app.get('/api/:table', (req, res) => {
-  const{producer, name} = req.params
-  connection.query('SELECT * FROM ' + req.params.table, (err, rows, fields) => {
-    if (err) throw err
-    res.send(rows)
-  })
-})
+app.get("/api/:table", (req, res) => {
+  const { producer, name } = req.params;
+  connection.query("SELECT * FROM " + req.params.table, (err, rows, fields) => {
+    if (err) throw err;
+    res.send(rows);
+  });
+});
 
-app.get('/api/:producer/:param', (req, res) => {
-  const{producer, param} = req.params
-  let requestName = ''
-  switch (producer){
-    case legalProducers.includes(producer) && producer :
-      requestName = 'color'
+app.get("/api/:producer/:param", (req, res) => {
+  const { producer, param } = req.params;
+  let requestName = "";
+  switch (producer) {
+    case legalProducers.includes(producer) && producer:
+      requestName = "color";
       break;
-    case 'chamfers':
-      requestName = 'type'   
+    case "chamfers":
+      requestName = "type";
       break;
-    default: return console.error('error in POST request')
-  } 
-    connection.query('SELECT * FROM ' + `${producer}` + ' WHERE ' + `${requestName}` + '=?', [param], (err, rows, fields) => {
-      if (err) throw err
-      res.send(rows)
-    })
-})
-
-app.get('/api/colors/:size/:producer', (req, res) => {
-  connection.query('SELECT ' + req.params.size + ' FROM ' + req.params.producer, (err, rows, fields) => {
-    if (err) throw err
-    res.send(rows)
-  }) 
-})
-
-
-app.post('/api/calc', async (req, res) => {
-  try{
-    const result = await calcService.calculate(req.body)
-    res.json(result)
-  } catch (err){
-    console.error(err)
-    res.status(500).json({ error: 'Server error' })
+    default:
+      return console.error("error in POST request");
   }
-})
+  connection.query(
+    "SELECT * FROM " + `${producer}` + " WHERE " + `${requestName}` + "=?",
+    [param],
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.send(rows);
+    }
+  );
+});
+
+app.get("/api/colors/:size/:producer", (req, res) => {
+  connection.query(
+    "SELECT " + req.params.size + " FROM " + req.params.producer,
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.send(rows);
+    }
+  );
+});
+
+app.post("/api/calc", async (req, res) => {
+  try {
+    const result = await calcService.calculate(req.body);
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode and listening on port ${PORT}`);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode and listening on port ${PORT}`
+  );
 });
